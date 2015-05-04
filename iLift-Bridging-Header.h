81379708d757e649d0bc6d6e28a19fb1e9a0c57a
Thomas Lixing Yang <tboneyang@gmail.com>
2015-05-04 13:19:04 -0400

    headerfiles added for coreplot

---
+++ iLift-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  iLift-Bridging-Header.h
+//  iChode
+//
+//  Created by Thomas Yang on 5/4/15.
+//  Copyright (c) 2015 Aryzle. All rights reserved.
+//
+
+#ifndef iChode_iLift_Bridging_Header_h
+#define iChode_iLift_Bridging_Header_h
+#import "CorePlot-CocoaTouch.h"
+
+#endif
