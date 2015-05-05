84cfed828400599f9e6e68d852abe5a51bc622e9
Thomas Lixing Yang <tboneyang@gmail.com>
2015-05-04 22:43:21 -0400

    cleaned up home-page buttons, added functionality to steppers and slider on workout view

--- iChode/ViewController4.swift
+++ iChode/ViewController4.swift
@@ -8,14 +8,50 @@
 
 import UIKit
 
-class ViewController4: UIViewController {
+var setN = 3
+var repN = 6
 
+
+class ViewController4: UIViewController {
+    //title of the selected Workout
     @IBOutlet weak var workoutTitle: UILabel!
+    
+    //Stepper for Sets
+    @IBOutlet weak var setButtonValue: UIStepper!
+    @IBOutlet weak var setLabel: UILabel!
+    @IBAction func setButton(sender: AnyObject) {
+        self.setLabel.text = Int(self.setButtonValue.value).description
+    }
+    
+    //Stepper for Reps
+    @IBOutlet weak var repLabel: UILabel!
+    @IBOutlet weak var repButtonValue: UIStepper!
+    @IBAction func repButton(sender: AnyObject) {
+        self.repLabel.text = Int(self.repButtonValue.value).description
+    }
+    
+    //Slider for Weight
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var weightSliderValue: UISlider!
+    @IBAction func weightSliderChange(sender: AnyObject) {
+        weightLabel.text = String(stringInterpolationSegment: Int(weightSliderValue.value * 300)) + " lbs"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         workoutTitle.text = workoutArray[activeMuscle][activeWorkout]
+        
+        setButtonValue.value = Double(setN)
+        setButtonValue.wraps = false
+        setButtonValue.autorepeat = false
+        setButtonValue.maximumValue = 9
+        
+        repButtonValue.value = Double(repN)
+        repButtonValue.wraps = false
+        repButtonValue.autorepeat = false
+        repButtonValue.maximumValue = 15
     }
 
     override func didReceiveMemoryWarning() {
